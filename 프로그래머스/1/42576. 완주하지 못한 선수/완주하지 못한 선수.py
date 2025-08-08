a60887dd6ee7b4c446fbe29412f3530cb8bc39bb
배준호 <114082026+BaeJunH0@users.noreply.github.com>
2025-08-08 20:06:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.29 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,6 +1,5 @@
 def solution(participant, completion):
     dic = {}
-    
     for i in completion:
         if i not in dic.keys():
             dic[i] = 1
@@ -8,11 +7,7 @@ def solution(participant, completion):
             dic[i] += 1
     
     for i in participant:
-        # 종료 조건 1. 완주자 명단에 없는 경우
-        if i not in dic.keys():
+        if i not in dic.keys() or dic[i] == 0:
             return i
-        
-        # 종료 조건 2. 동명이인이 완주자 명단에 있는 경우
-        dic[i] -= 1
-        if dic[i] < 0:
-            return i
+        else:
+            dic[i] -= 1
