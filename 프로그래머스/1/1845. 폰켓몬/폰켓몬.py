9ac2000f688df3600198bc4e0e4e06e9d7ec7a31
배준호 <114082026+BaeJunH0@users.noreply.github.com>
2025-08-08 20:27:38 +0900

    [level 1] Title: 폰켓몬, Time: 2.44 ms, Memory: 9.76 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,12 @@
 def solution(nums):
     dic = {}
     for i in nums:
-        if i in dic.keys():
-            dic[i] += 1
-        else:
+        if i not in dic.keys():
             dic[i] = 1
-    want = len(nums) // 2
-    length = len(list(dic.keys()))
-    return length if length <= want else want
+        else:
+            dic[i] += 1
+    
+    if len(dic.keys()) < len(nums) // 2:
+        return len(dic.keys())
+    else:
+        return len(nums) // 2
