5047c4c2644b7a6e68c37e5441ff33b32d12f54a
배준호 <114082026+BaeJunH0@users.noreply.github.com>
2025-09-29 15:04:27 +0900

    [Silver II] Title: 섬의 개수, Time: 48 ms, Memory: 32760 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,18 +1,16 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
 
-def bfs():
-    while queue:
-        x, y = queue.popleft()
+def dfs(x, y):
+    for a, b in dir:
+        nx, ny = x + a, y + b
 
-        for a, b in dir:
-            nx, ny = x + a, y + b
-            if 0 <= nx < h and 0 <= ny < w and not check[nx][ny]:
-                if sea[nx][ny] == 1:
-                    queue.append((nx, ny))
-                    check[nx][ny] = True
+        if 0 <= nx < h and 0 <= ny < w and not check[nx][ny]:
+            if sea[nx][ny] == 1:
+                check[nx][ny] = True
+                dfs(nx, ny)
 
 while True:
     w, h = map(int, input().rstrip().split())
@@ -23,7 +21,6 @@ while True:
     sea = []
     check = [[False] * w for _ in range(h)]
     dir = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
-    queue = deque()
 
     for i in range(h):
         line = list(map(int, input().rstrip().split()))
@@ -35,7 +32,6 @@ while True:
             if sea[i][j] == 1 and not check[i][j]:
                 count += 1
                 check[i][j] = True
-                queue.append((i, j))
-                bfs()
+                dfs(i, j)
     
     print(count)
